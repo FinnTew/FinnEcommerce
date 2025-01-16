3533b0fba673925b967903a8e4d599f62909a863
FinnTew <nanomoa76@gmail.com>
2025-01-16 18:46:57 +0800

    implement client for auth service

--- src/cmd/auth/main.go
+++ src/cmd/auth/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"context"
+	authk "github.com/FinnTew/FinnEcommerce/src/internal/client/kitex_gen/auth"
+	"github.com/FinnTew/FinnEcommerce/src/internal/client/rpc/auth"
+	"github.com/cloudwego/kitex/client"
+	consul "github.com/kitex-contrib/registry-consul"
+	"log"
+)
+
 func main() {
+	r, err := consul.NewConsulResolver("127.0.0.1:8500")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cli, err := auth.NewRPCClient("auth", client.WithResolver(r))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := cli.DeliverTokenByRPC(
+		context.TODO(),
+		&authk.DeliverTokenReq{
+			UserId: 1,
+		},
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(res)
 
 }
